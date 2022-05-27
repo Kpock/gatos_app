a5bad27841463c80429d09b6b805f23f37c2f066
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-05-27 17:02:36 -0500

    avanzando gatos
    continuamos gatos

---
+++ gatos_app/src/main/java/com/brandon/gatos_app/Gatos.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.brandon.gatos_app;
+
+/**
+ *
+ * @author WPOSS
+ */
+public class Gatos {
+    int id;
+    String url;
+    String apikey = "630640b1-7703-47e9-a518-6ee78442d1b7";
+    String image;
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+    
+    
+    
+    
+}
